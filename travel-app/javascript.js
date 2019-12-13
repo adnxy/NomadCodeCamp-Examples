f4ba133e403c39bb8dde67d0885317647b5bbe9c
Adnan Sahinovic <a.sahinovic@yahoo.com>
2019-12-13 09:20:30 +0700

    Refactoring add to album function

--- travel-app/javascript.js
+++ travel-app/javascript.js
@@ -1,32 +1,11 @@
-function addToFirstAlbum() {
+function addToAlbum(abumClass, cityClass, buttonClass) {
 	// Select album
-	var albumPhotos = document.querySelector('.first-album-container');
+	var albumPhotos = document.querySelector(abumClass);
 	// Select pictures
-	var cityPhotos = document.querySelector('.images-container-chiang-mai');
+	var cityPhotos = document.querySelector(cityClass);
 	// Replace pictures with album
 	albumPhotos.parentNode.replaceChild(cityPhotos, albumPhotos);
-	// Remove button Add to Album 1
-	document.querySelector('.add-button-first-album').style.display = 'none';
+	// Remove button Add to Album 
+	document.querySelector(buttonClass).style.display = 'none';
 }
 
-function addToSecondAlbum() {
-	// Select album
-	var albumPhotos = document.querySelector('.second-album-container');
-	// Select pictures
-	var cityPhotos = document.querySelector('.images-container-prague');
-	// Replace pictures with album
-	albumPhotos.parentNode.replaceChild(cityPhotos, albumPhotos);
-	// Remove button Add to Album 2
-	document.querySelector('.add-button-second-album').style.display = 'none';
-}
-
-function addToThirdAlbum() {
-	// Select album
-	var albumPhotos = document.querySelector('.third-album-container');
-	// Select pictures
-	var cityPhotos = document.querySelector('.images-container-medellin');
-	// Replace pictures with album
-	albumPhotos.parentNode.replaceChild(cityPhotos, albumPhotos);
-	// Remove button Add to Album 3
-	document.querySelector('.add-button-third-album').style.display = 'none';
-}
